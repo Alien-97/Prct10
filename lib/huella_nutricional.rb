163364fad89a052641ca6087514ec2fd5d5d4f26
Alien-97 <alu0101035406@ull.edu.es>
2020-01-10 09:40:53 +0000

    Añadido el fichero travis

--- lib/huella_nutricional.rb
+++ lib/huella_nutricional.rb
@@ -4,7 +4,7 @@ require "huella_nutricional/lista"
 require "huella_nutricional/PlatoHuellaNutricional"
 require "huella_nutricional/PlatoHuellaAmbiental"
 require "huella_nutricional/menu"
-#ddd
+#ddd añadido el travis
 module HuellaNutricional
   class Error < StandardError; end
   # Your code goes here...
